1c995ac2cd6dc6c094aaf6d064f239ebebd23b02
idlebxx <idlebxx3@gmail.com>
2026-08-15 23:48:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const address = 'https://www.google.com'
 const app = express();
 const appServer = http.createServer(app);
 const appSocket = new webSocket.Server({server: appServer});
-const appBot = new telegramBot(token, {polling: true});
+const appBot = new TelegramBot(token, {polling: true});
 const appClients = new Map()
 
 const upload = multer();
